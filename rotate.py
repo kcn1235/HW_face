67f66b54a6e92eacbd899bbde6630c899e40bc16
kcn1235 <390037635@qq.com>
2018-06-05 17:50:50 +0800

    Add files via upload

--- rotate.py
+++ rotate.py
@@ -1,13 +1,39 @@
 from PIL import Image  
+from skimage import data, exposure, img_as_float
 import os
+import numpy as np
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 
 for i in range(2000):
 	im = Image.open(dirname + '/%d/%d_1.jpg' % (i,i))  
-	im_rotate = im.rotate(90)
-	im_rotate.save(dirname + '/%d/%d_2.jpg' % (i,i))
-	im_rotate = im.rotate(180)
-	im_rotate.save(dirname + '/%d/%d_3.jpg' % (i,i))
-	im_rotate = im.rotate(270)
-	im_rotate.save(dirname + '/%d/%d_4.jpg' % (i,i))
+	gam1 = exposure.adjust_gamma(im, 2)
+	gam2 = exposure.adjust_gamma(im, 0.5)
+	im_Contrast = skimage.exposure.rescale_intensity(im)
+	gam1.save(dirname + '/%d/%d_2.jpg' % (i,i))
+	gam2.save(dirname + '/%d/%d_3.jpg' % (i,i))
+	im_Contrast.save(dirname + '/%d/%d_4.jpg' % (i,i))
+	im_noise = im
+	rows,cols,dims=im_noise.shape
+	for i in range(500):    
+		x=np.random.randint(0,rows)
+		y=np.random.randint(0,cols)
+		im_noise[x,y,:]=255
+	im_noise.save(dirname + '/%d/%d_5.jpg' % (i,i))
+
+
+	im_transpose = im.transpose(Image.FLIP_LEFT_RIGHT)
+	im_transpose.save(dirname + '/%d/%d_6.jpg' % (i,i))
+	gam1 = exposure.adjust_gamma(im_transpose, 2)
+	gam2 = exposure.adjust_gamma(im_transpose, 0.5)
+	im_Contrast = skimage.exposure.rescale_intensity(im_transpose)
+	gam1.save(dirname + '/%d/%d_7.jpg' % (i,i))
+	gam2.save(dirname + '/%d/%d_8.jpg' % (i,i))
+	im_Contrast.save(dirname + '/%d/%d_9.jpg' % (i,i))
+	im_noise = im_transpose
+	rows,cols,dims=im_noise.shape
+	for i in range(500):    
+		x=np.random.randint(0,rows)
+		y=np.random.randint(0,cols)
+		im_noise[x,y,:]=255
+	im_noise.save(dirname + '/%d/%d_10.jpg' % (i,i))
