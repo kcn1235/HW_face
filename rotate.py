a0b9a15006a7e956f45aea7fbd5e9087a0951c8a
kcn1235 <390037635@qq.com>
2018-06-05 16:21:42 +0800

    Add files via upload

--- rotate.py
+++ rotate.py
@@ -1,11 +1,13 @@
 from PIL import Image  
 import os
 
+dirname = os.path.dirname(os.path.abspath(__file__))
+
 for i in range(2000):
-	im = Image.open('/%d/%d_1.jpg' % i, % i)  
+	im = Image.open(dirname + '/%d/%d_1.jpg' % (i,i))  
 	im_rotate = im.rotate(90)
-	im_rotate.save('d:/%d_2.jpg')
+	im_rotate.save(dirname + '/%d/%d_2.jpg' % (i,i))
 	im_rotate = im.rotate(180)
-	im_rotate.save('d:/%d_3.jpg')
+	im_rotate.save(dirname + '/%d/%d_3.jpg' % (i,i))
 	im_rotate = im.rotate(270)
-	im_rotate.save('d:/%d_4.jpg')
+	im_rotate.save(dirname + '/%d/%d_4.jpg' % (i,i))
